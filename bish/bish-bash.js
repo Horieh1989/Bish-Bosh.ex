3976e8a47df0e7910eb8d5f0fa63bb75d7da66f7
Horieh Ghiasidalalian <horiehghiasi@gmail.com>
2023-10-25 11:52:48 +0200

    fixing with bish-bosh index html

--- bish/bish-bash.js
+++ bish/bish-bash.js
@@ -1,19 +1,21 @@
 function runBishBosh() {
     const number = parseInt(document.getElementById("input").value);
+    const bishValue = parseInt(document.getElementById("bish").value);
+    const boshValue = parseInt(document.getElementById("bosh").value);
    
     let output = '';
 
     for (let i = 1; i <= number; i++) {
         if (i % number === 0 ) {
             output += "Bish-Bosh, ";
-        } else if (i % number === 3) {
+        } else if (i % bishValue === 0) {
             output += "Bish, ";
-        } else if (i % number === 4) {
+        } else if (i % boshValue === 0) {
             output += "Bosh, ";
         } else {
             output += i + ", ";
         }
     }
-
+    console.log(output);
     document.getElementById("output").textContent = output.slice(0, -2); // Remove the trailing comma and space
 }
